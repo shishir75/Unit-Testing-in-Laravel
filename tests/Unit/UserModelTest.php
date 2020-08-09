f07dd525eb1e22a5822f330eb22e58e682761ee4
shishir75 <shishir.srdr16@gmail.com>
2020-08-09 15:30:11 +0600

    Database Transaction in laravel Testing

---
+++ tests/Unit/UserModelTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;  // it is very important
+
+class UserModelTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+    * A basic test.
+    * @test
+    * @return void
+    */
+    public function user_has_full_name_attribute()
+    {
+        $user = User::create(['first_name' => 'Shishir', 'last_name' => 'Sarder', 'email' => 'me@abc.com', 'password' => 'password']);
+
+        $this->assertEquals('Shishir Sarder', $user->fullname);
+    }
+}
